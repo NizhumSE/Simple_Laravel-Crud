3d9e3e8c8150ded3c2c8010744d54c3524c6e381
unknown <nizhumdaru50@gmail.com>
2025-01-04 18:23:24 +0600

    Completed a simple laravel crud project

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Posts</h1>
+    <a href="{{ route('posts.create') }}" class="btn btn-primary mb-3">Create Post</a>
+    @if(session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @endif
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Content</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($posts as $post)
+                <tr>
+                    <td>{{ $post->id }}</td>
+                    <td>{{ $post->title }}</td>
+                    <td>{{ $post->content }}</td>
+                    <td>
+                        <a href="{{ route('posts.edit', $post) }}" class="btn btn-warning">Edit</a>
+                        <form action="{{ route('posts.destroy', $post) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
